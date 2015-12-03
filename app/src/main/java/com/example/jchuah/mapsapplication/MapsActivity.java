865794f917f27bffb7d68f9eb1ef6f0f13e092f5
Joon Yee Chuah <joonyee.chuah@austinisd.org>
2015-12-03 12:19:42 -0600

    Changed marker icon

--- app/src/main/java/com/example/jchuah/mapsapplication/MapsActivity.java
+++ app/src/main/java/com/example/jchuah/mapsapplication/MapsActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -60,6 +61,11 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.addMarker(
+                new MarkerOptions()
+                        .position(new LatLng(0, 0))
+                        .title("Marker")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.cast_ic_notification_0))
+        );
     }
 }
